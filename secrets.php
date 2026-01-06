7054bb6089a90cca30473e9f711457aeb45d8c9a
no-cost <info@no-cost.site>
2026-01-06 17:06:23 +0000

    change file modes

--- secrets.php
+++ secrets.php
@@ -1,11 +1,11 @@
 <?php
-$main_domain = '...';
+$main_domain = "no-cost.site";
 
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         "26DC6374c5b79cE4A1e6A58Fa9EdF6ED");
+define('SECURE_AUTH_KEY',  "F56e1AA5D2604Aaa8EEAEFcc2cCEB0cC");
+define('LOGGED_IN_KEY',    "49bfEDea1eeF6AF4fd6F8C31e3B2c2Db");
+define('NONCE_KEY',        "bc25f5Db2F2Bd03Bbc2CcE2BE35b1d6b");
+define('AUTH_SALT',        "80E1C88cE2cCb89cFE3eaEeb6E3eEEDa");
+define('SECURE_AUTH_SALT', "F6d9DB8Cd5b5CCED9eC4A35e5Bd7B6AD");
+define('LOGGED_IN_SALT',   "Bae062B2ba09ED61ED86FDBFb2D7eA5E");
+define('NONCE_SALT',       "6ba68baa4290583bdb2c0402FbCdC9C1");
