29153c9cb632bda287b9a600a8655778ea4ea187
Kevin Svitač <kevin@svit.ac>
2026-03-23 21:30:32 +0100

    remove plugin

--- wp-content/plugins/event-tickets-with-ticket-scanner/saso-eventtickets-validator.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function () {
-	let myAjax = Ajax_sasoEventtickets;
-	if (jQuery('#'+myAjax.divId)) {
-		jQuery.getScript( myAjax.jsFiles, function( data, textStatus, jqxhr ){});
-	}
-} );
--- wp-content/plugins/event-tickets-with-ticket-scanner/ticket/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    require_once('../../../../wp-load.php');
-	include_once "../sasoEventtickets_Ticket.php";
-	$vollstart_Ticket = sasoEventtickets_Ticket::Instance($_SERVER["REQUEST_URI"]);
-	$vollstart_Ticket->initFilterAndActions();
-	$vollstart_Ticket->renderPage();
-
-?>
